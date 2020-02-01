03252aaf77fdd9a2669904b1c97488d515e0d5ac
Olga Miliukova <cogito_ergo_summ@mail.ru>
2020-02-01 23:25:07 +0300

    Added cors for the frontend

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,11 +1,13 @@
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const helmet = require('helmet');
 
 const auth = require('./auth');
 const logs = require('./logs');
 // Middlewars
 module.exports = (app) => {
+  app.use(cors());
   app.use(helmet());
   app.use(cookieParser());
   app.use(bodyParser.json());
